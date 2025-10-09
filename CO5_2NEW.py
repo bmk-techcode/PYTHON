6334636db96c7eba21eb129574f2d51ba1691324
bmk-techcode <beviofficial123@gmail.com>
2025-10-08 23:52:23 -0700

    Add files via upload
    COPYING EVEN AND ODD LINES

---
+++ CO5_2NEW.py
@@ -0,0 +1,18 @@
+ofile=open("file.txt","r")
+content=ofile.readlines()
+l=len(content)
+
+feven=open("evenfile.txt","w")
+fodd=open("oddfile.txt","w")
+
+for i in range(l):
+    if i%2==0:
+        
+        feven.write(content[i])
+
+    else:
+        fodd.write(content[i])
+print("FILE UPDATED ....")
+ofile.close()
+feven.close()
+fodd.close()
